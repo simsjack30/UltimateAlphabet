c25b942b466ca7d9816954e0183b664cb484b73f
John <simsjack30@gmail.com>
2024-08-16 15:59:23 -0500

    feat: Update image magnifier functionality
    This commit updates the image magnifier functionality by adding the ability to toggle the magnifier on and off with a button click. It introduces a new `handleClick` function that toggles the `mag` variable to control the display of the magnifier. When the magnifier is enabled, it is displayed on mouse hover as before. When the magnifier is disabled, it is hidden. This provides a more user-friendly experience by allowing users to choose whether they want to use the magnifier or not.

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,8 +1,7 @@
 <script lang="ts">
 	import PocketBase from 'pocketbase';
-	import { popup } from '@skeletonlabs/skeleton';
 	import type { PopupSettings } from '@skeletonlabs/skeleton';
-	import { Check } from 'lucide-svelte';
+	import { Check, SearchX, SearchCheck, Search } from 'lucide-svelte';
 
 	const pb = new PocketBase('https://pocketbase-production-c5bc.up.railway.app');
 	export let data;
@@ -38,70 +37,71 @@
 		}
 	}
 
-	const popupHover: PopupSettings = {
-		event: 'hover',
-		target: 'popupHover',
-		placement: 'top'
-	};
-
 	let magnifier;
 	let isInsideImage = false;
+	let mag = true;
 
 	function handleMouseEnter() {
 		isInsideImage = true;
 		magnifier.style.display = 'block';
 	}
 
-	function handleMouseLeave() {
-		isInsideImage = false;
-	}
-
 	function handleMouseMove(event) {
-		const img = document.querySelector('.image-container img'); // Get the image element
-		const { top, left, width, height } = img.getBoundingClientRect();
-		let x = event.clientX - left;
-		let y = event.clientY - top;
+		if (mag) {
+			const img = document.querySelector('.image-container img'); // Get the image element
+			const { top, left, width, height } = img.getBoundingClientRect();
+			let x = event.clientX - left;
+			let y = event.clientY - top;
 
-		// Update the background position percentages for the zoomed background
-		let xPercent = (x / width) * 100;
-		let yPercent = (y / height) * 100;
+			// Update the background position percentages for the zoomed background
+			let xPercent = (x / width) * 100;
+			let yPercent = (y / height) * 100;
 
-		// Constrain magnifier within the image for x-axis and y-axis
-		let magnifierX = x - magnifier.offsetWidth / 2;
-		let magnifierY = y - magnifier.offsetHeight / 2;
+			// Constrain magnifier within the image for x-axis and y-axis
+			let magnifierX = x - magnifier.offsetWidth / 2;
+			let magnifierY = y - magnifier.offsetHeight / 2;
 
-		if (magnifierX < 0) magnifierX = 0;
-		if (magnifierX + magnifier.offsetWidth > width) magnifierX = width - magnifier.offsetWidth;
-		if (magnifierY < 0) magnifierY = 0;
-		if (magnifierY + magnifier.offsetHeight > height) magnifierY = height - magnifier.offsetHeight;
+			if (magnifierX < 0) magnifierX = 0;
+			if (magnifierX + magnifier.offsetWidth > width) magnifierX = width - magnifier.offsetWidth;
+			if (magnifierY < 0) magnifierY = 0;
+			if (magnifierY + magnifier.offsetHeight > height)
+				magnifierY = height - magnifier.offsetHeight;
 
-		// Ensure that the magnifier moves freely but the background image position stops when leaving the image
-		if (x < 0 || x > width) {
-			xPercent = x < 0 ? 0 : 100; // Lock the background to the edges horizontally
+			// Ensure that the magnifier moves freely but the background image position stops when leaving the image
+			if (x < 0 || x > width) {
+				xPercent = x < 0 ? 0 : 100; // Lock the background to the edges horizontally
+			}
+			if (y < 0 || y > height) {
+				yPercent = y < 0 ? 0 : 100; // Lock the background to the edges vertically
+			}
+
+			// Position the magnifier
+			magnifier.style.left = `${magnifierX}px`;
+			magnifier.style.top = `${magnifierY}px`;
+
+			// Update the zoomed-in background image position, locking it when outside
+			magnifier.style.backgroundImage = `url('${img.src}')`;
+			magnifier.style.backgroundPosition = `${xPercent}% ${yPercent}%`;
 		}
-		if (y < 0 || y > height) {
-			yPercent = y < 0 ? 0 : 100; // Lock the background to the edges vertically
-		}
-
-		// Position the magnifier
-		magnifier.style.left = `${magnifierX}px`;
-		magnifier.style.top = `${magnifierY}px`;
-
-		// Update the zoomed-in background image position, locking it when outside
-		magnifier.style.backgroundImage = `url('${img.src}')`;
-		magnifier.style.backgroundPosition = `${xPercent}% ${yPercent}%`;
 	}
 
 	function handleGlobalMouseMove(event) {
 		handleMouseMove(event);
 	}
+
+	function handleClick() {
+		mag = !mag;
+		if (mag) {
+			magnifier.style.display = 'block';
+		}
+	}
 </script>
 
+<!-- svelte-ignore a11y-no-static-element-interactions -->
 <div
-	class="flex flex-col lg:justify-between items-center lg:items-start w-full h-screen lg:flex-row-reverse"
+	class="flex flex-col lg:justify-between xl:justify-evenly items-center lg:items-start w-full h-screen lg:flex-row-reverse"
 	on:mousemove={handleGlobalMouseMove}
 	on:mouseenter={handleMouseEnter}
-	on:mouseleave={handleMouseLeave}
 >
 	<div class="lg:hidden flex flex-row gap-4 pt-2 md:pt-4 items-center">
 		<button class="btn variant-filled-secondary rounded-md shadow-xl">
@@ -111,19 +111,26 @@
 		</button>
 		<h3 class="h3 whitespace-nowrap">Ultimate Alphabet</h3>
 	</div>
+
 	<div class="p-2 md:p-4 h-full">
 		<div class="lg:h-full max-h-full image-container">
 			<img
 				src="wilks2.jpg"
 				alt=""
-				class="rounded-lg max-h-full w-auto object-top object-contain shadow-lg cursor-none"
+				class="rounded-lg max-h-full w-auto object-top object-contain shadow-lg {mag
+					? 'cursor-none'
+					: 'cursor-auto'}"
 			/>
-			<div bind:this={magnifier} class="magnifier rounded-lg"></div>
+			<div class="hidden lg:flex">
+				{#if mag}
+					<div bind:this={magnifier} class="magnifier rounded-lg"></div>
+				{/if}
+			</div>
 		</div>
 	</div>
 
 	<div
-		class="flex flex-col m-2 md:m-4 w-full lg:w-auto lg:items-start lg:justify-start items-center gap-4"
+		class="flex flex-col m-2 md:m-4 w-full lg:w-[40rem] lg:items-start lg:justify-start items-center gap-4"
 	>
 		<div class="hidden lg:flex flex-row gap-4 items-center">
 			<button class="btn variant-filled-secondary shadow-xl rounded-md">
@@ -133,6 +140,20 @@
 			</button>
 			<h3 class="h3 whitespace-nowrap">Ultimate Alphabet</h3>
 		</div>
+
+		<div class="lg:flex hidden">
+			<button
+				class="icon btn {mag ? 'variant-ghost' : 'variant-filled-secondary'} rounded-md"
+				on:click={handleClick}
+			>
+				{#if mag}
+					<SearchX />
+				{:else}
+					<SearchCheck />
+				{/if}
+			</button>
+		</div>
+
 		<div class="flex flex-row gap-2 md:gap-4">
 			<input
 				class="flex input rounded-lg w-64 variant-filled shadow-xl"
@@ -152,15 +173,17 @@
 				successful guess.
 			</h5>
 		{/if}
-		<ul
-			class="px-2 pb-2 md:p-4 lg:m-auto lg:p-0 grid grid-cols-2 md:grid-cols-3 lg:grid-cols-1 xl:grid-cols-2 gap-x-2 md:gap-x-4 lg:gap-x-6 gap-y-2"
-		>
-			{#each guessedItems.slice().reverse() as guessed}
-				<li class="px-2 py-1 rounded variant-ghost">
-					<strong>{guessed.item}</strong> - {guessed.note}
-				</li>
-			{/each}
-		</ul>
+		<div class="flex justify-start items-start">
+			<ul
+				class="px-2 pb-2 md:p-4 lg:m-auto lg:p-0 grid grid-cols-2 md:grid-cols-3 lg:grid-cols-1 xl:grid-cols-2 gap-x-2 md:gap-x-4 lg:gap-x-6 gap-y-2"
+			>
+				{#each guessedItems.slice().reverse() as guessed}
+					<li class="px-2 py-1 rounded variant-ghost">
+						<strong>{guessed.item}</strong> - {guessed.note}
+					</li>
+				{/each}
+			</ul>
+		</div>
 	</div>
 </div>
 
